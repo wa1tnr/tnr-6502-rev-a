146f19d2670256d3f3da067d8810967c86e0bed8
wa1tnr <wa1tnr@users.noreply.github.com>
2023-09-20 17:26:14 +0000

    Great -99, AAAA  mod see comment
      The program prints -99, then AAAA forever.
    
      As planned.  So something is very much working.
    
    	modified:   progress-aa.asm
    
    On branch dvlp-aa

--- asm-aab.d/progress-aa.asm
+++ asm-aab.d/progress-aa.asm
@@ -1,4 +1,4 @@
-; 17:09:52z - 
+; 17:23:22z - 
 
 ; wed 20 sep 2023
 
@@ -11,14 +11,26 @@ REDO      LDA #$2F  ;  K > A ; PROVEN
           LDA #$3B  ;  K > A ; PROVEN
           STA $401  ;  A > M ; PROVEN
 
-LOOP      TAX
+          LDA #$2D  ; minus
           JSR OUTCH
-          STX $402
-          STY $403
-          JSR DELAY  ; did not crash
-          LDX $402
-          LDY $403
-          INX
+          LDA #$39  ; 9
+          JSR OUTCH
+          LDA #$39
+          JSR OUTCH
+          LDA #$2C  ; comma
+          JSR OUTCH
+          LDA #$20  ; space
+          JSR OUTCH
+          LDA #$41  ;  'A'
+
+LOOP      TAX
+          JSR OUTCH ;  A > DSP ; PROVEN
+          STX $402  ;  X > M ; somewhat proven
+          STY $403  ;  Y > M ; again somewhat proven
+          JSR DELAY ;  returns or seems to
+          LDX $402  ;  M > X ; might be working
+          LDY $403  ;  M > Y ; again might be working
+          ; INX
           TXA
           JMP LOOP
 
@@ -40,20 +52,20 @@ LOOPD     DEX
 
 
 ; 0300: A9 2F 8D 00 04 A9 3B 8D
-; 0308: 01 04 AA 20 EF FF 8E 02
-; 0310: 04 8C 03 04 20 25 03 AE
-; 0318: 02 04 AC 03 04 E8 8A 4C
-; 0320: 0A 03 4C 00 03 A0 C7 A2
-; 0328: D9 CA D0 FD 88 D0 FA 60
-; 0330: 4C 00 03 4C 25 03
+
+
+; aaa
+0300: A9 2F 8D 00 04 A9 3B 8D
+0308: 01 04 A9 2D 20 EF FF A9
+0310: 39 20 EF FF A9 39 20 EF
+0318: FF A9 2C 20 EF FF A9 20
+0320: 20 EF FF A9 41 AA 20 EF
+0328: FF 8E 02 04 8C 03 04 20
+0330: 3F 03 AE 02 04 AC 03 04
+0338: 8A 4C 25 03 4C 00 03 A0
+0340: C7 A2 D9 CA D0 FD 88 D0
+0348: FA 60 4C 00 03 4C 3F 03
+
 
 ; end.
-
-0300: A9 2F 8D 00 04 A9 3B 8D
-0308: 01 04 AA 20 EF FF 8E 02
-0310: 04 8C 03 04 20 25 03 AE
-0318: 02 04 AC 03 04 E8 8A 4C
-0320: 0A 03 4C 00 03 A0 C7 A2
-0328: D9 CA D0 FD 88 D0 FA 60
-0330: 4C 00 03 4C 25 03
 

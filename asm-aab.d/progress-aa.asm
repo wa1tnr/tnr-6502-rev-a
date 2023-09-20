c90a22a063d03b51e57c3581c92007bb655661ed
wa1tnr <wa1tnr@users.noreply.github.com>
2023-09-20 16:53:55 +0000

    good

--- asm-aab.d/progress-aa.asm
+++ asm-aab.d/progress-aa.asm
@@ -3,9 +3,10 @@
 
 OUTCH       = $FFEF
 
-REDO      LDA #$2F  ;  K > A
-          STA $400  ;  A > M
-
+REDO      LDA #$2F  ;  K > A ; PROVEN
+          STA $400  ;  A > M ; PROVEN
+          LDA #$3B  ;  K > A
+          STA $401  ;  A > M
 LOOP      TAX
           JSR OUTCH
 ;         JSR DELAY
@@ -28,9 +29,9 @@ LOOPD     DEX
           JMP DELAY
          .END
 
-0300: A9 2F 8D 00 04 AA 20 EF
-0308: FF E8 8A 4C 05 03 4C 00
-0310: 03 A0 07 A2 07 60 CA D0
-0318: FD 88 D0 FA 60 4C 00 03
-0320: 4C 11 03
+; 0300: A9 2F 8D 00 04 A9 3B 8D
+; 0308: 01 04 AA 20 EF FF E8 8A
+; 0310: 4C 0A 03 4C 00 03 A0 07
+; 0318: A2 07 60 CA D0 FD 88 D0
+; 0320: FA 60 4C 00 03 4C 16 03
 

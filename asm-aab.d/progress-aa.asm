123cace30a70997257f285c69fad91f7d2f38c53
wa1tnr <wa1tnr@users.noreply.github.com>
2023-09-20 18:45:52 +0000

    recovery from problem DONE

--- asm-aab.d/progress-aa.asm
+++ asm-aab.d/progress-aa.asm
@@ -1,4 +1,4 @@
-; 17:29:53z -  ONLINE edit
+; 18:28:22z - OFFLINE edit
 
 ; wed 20 sep 2023
 
@@ -6,8 +6,17 @@
 
 OUTCH       = $FFEF
 
+BEGIN     JMP REDO
 
 PREPRINT
+          LDA #$8D
+          JSR OUTCH
+          LDA #$8D
+          JSR OUTCH
+          LDA #$20  ; space
+          JSR OUTCH
+          LDA #$20
+          JSR OUTCH
           LDA #$2D  ; minus
           JSR OUTCH
           LDA #$39  ; 9
@@ -16,26 +25,26 @@ PREPRINT
           JSR OUTCH
           LDA #$2C  ; comma
           JSR OUTCH
-          LDA #$20  ; space
+          LDA #$20
           JSR OUTCH
           RTS
 
-REDO      LDA #$2F  ;  K > A ; PROVEN
-          STA $400  ;  A > M ; PROVEN
-          LDA #$3B  ;  K > A ; PROVEN
-          STA $401  ;  A > M ; PROVEN
+REDO      LDA #$2F  ;  K > A
+          STA $400  ;  A > M
+          LDA #$3B
+          STA $401
 
           JSR PREPRINT
 
           LDA #$41  ;  'Q'
 
 LOOP      TAX
-          JSR OUTCH ;  A > DSP ; PROVEN
-          STX $402  ;  X > M ; somewhat proven
-          STY $403  ;  Y > M ; again somewhat proven
-          JSR DELAY ;  returns or seems to
-          LDX $402  ;  M > X ; might be working
-          LDY $403  ;  M > Y ; again might be working
+          JSR OUTCH ;  A > DSP
+          STX $402  ;  X > M
+          STY $403  ;  Y > M
+          JSR DELAY
+          LDX $402  ;  M > X
+          LDY $403  ;  M > Y
           ; INX
           TXA
           JMP LOOP
@@ -44,7 +53,6 @@ LOOP      TAX
 
 DELAY     LDY #$C7   ;  K > Y ; unproven
           LDX #$D9   ;  K > X ; unproven
-          ; RTS         really should run fine
 
 LOOPD     DEX
           BNE LOOPD
@@ -52,21 +60,27 @@ LOOPD     DEX
           BNE LOOPD
           RTS
 
-          JMP REDO
-          JMP DELAY
+          NOP
+          NOP
+          NOP
+;          JMP REDO
+;          JMP DELAY
          .END
 
 ; end.
 
-0300: A9 2D 20 EF FF A9 39 20
-0308: EF FF A9 39 20 EF FF A9
-0310: 2C 20 EF FF A9 20 20 EF
-0318: FF 60 A9 2F 8D 00 04 A9
-0320: 3B 8D 01 04 20 00 03 A9
-0328: 41 AA 20 EF FF 8E 02 04
-0330: 8C 03 04 20 43 03 AE 02
-0338: 04 AC 03 04 8A 4C 29 03
-0340: 4C 1A 03 A0 C7 A2 D9 CA
-0348: D0 FD 88 D0 FA 60 4C 1A
-0350: 03 4C 43 03
+
+0300: 4C 31 03 A9 8D 20 EF FF
+0308: A9 8D 20 EF FF A9 20 20
+0310: EF FF A9 20 20 EF FF A9
+0318: 2D 20 EF FF A9 39 20 EF
+0320: FF A9 39 20 EF FF A9 2C
+0328: 20 EF FF A9 20 20 EF FF
+0330: 60 A9 2F 8D 00 04 A9 3B
+0338: 8D 01 04 20 03 03 A9 41
+0340: AA 20 EF FF 8E 02 04 8C
+0348: 03 04 20 5A 03 AE 02 04
+0350: AC 03 04 8A 4C 40 03 4C
+0358: 31 03 A0 C7 A2 D9 CA D0
+0360: FD 88 D0 FA 60 EA EA EA
 

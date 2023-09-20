3cec16fe6e93c5b6614307086ac22a56a676d560
wa1tnr <wa1tnr@users.noreply.github.com>
2023-09-20 17:11:59 +0000

    FUNCTIONING DELAY W00T

--- asm-aab.d/progress-aa.asm
+++ asm-aab.d/progress-aa.asm
@@ -1,4 +1,4 @@
-; 17:01:58z - WORKS
+; 17:09:52z - 
 
 ; wed 20 sep 2023
 
@@ -15,7 +15,7 @@ LOOP      TAX
           JSR OUTCH
           STX $402
           STY $403
-          JSR DELAY
+          JSR DELAY  ; did not crash
           LDX $402
           LDY $403
           INX
@@ -24,9 +24,9 @@ LOOP      TAX
 
           JMP REDO
 
-DELAY     LDY #$7   ;  K > Y ; unproven
-          LDX #$7   ;  K > X ; unproven
-          RTS       ;  really should run fine
+DELAY     LDY #$C7   ;  K > Y ; unproven
+          LDX #$D9   ;  K > X ; unproven
+          ; RTS         really should run fine
 
 LOOPD     DEX
           BNE LOOPD
@@ -38,12 +38,22 @@ LOOPD     DEX
           JMP DELAY
          .END
 
+
 ; 0300: A9 2F 8D 00 04 A9 3B 8D
 ; 0308: 01 04 AA 20 EF FF 8E 02
 ; 0310: 04 8C 03 04 20 25 03 AE
 ; 0318: 02 04 AC 03 04 E8 8A 4C
-; 0320: 0A 03 4C 00 03 A0 07 A2
-; 0328: 07 60 CA D0 FD 88 D0 FA
-; 0330: 60 4C 00 03 4C 25 03
+; 0320: 0A 03 4C 00 03 A0 C7 A2
+; 0328: D9 CA D0 FD 88 D0 FA 60
+; 0330: 4C 00 03 4C 25 03
 
 ; end.
+
+0300: A9 2F 8D 00 04 A9 3B 8D
+0308: 01 04 AA 20 EF FF 8E 02
+0310: 04 8C 03 04 20 25 03 AE
+0318: 02 04 AC 03 04 E8 8A 4C
+0320: 0A 03 4C 00 03 A0 C7 A2
+0328: D9 CA D0 FD 88 D0 FA 60
+0330: 4C 00 03 4C 25 03
+
